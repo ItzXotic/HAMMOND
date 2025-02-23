db689ee394d03ccc5806ad2cad399e6cf8e49545
ItzXotic <88778423+ItzXotic@users.noreply.github.com>
2025-02-23 17:49:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
+      name: "Andrew",
       password: "enteryourcustompass",
       host: "180.188.226.76",
       port:  7019,
